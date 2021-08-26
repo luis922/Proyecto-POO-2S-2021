10c842752353c38b50118e929158411b5826fe1c
LaVolpeChalo <Sebastianlavolpe@gmail.com>
2021-08-26 18:31:04 -0400

    Interfaz inicial
    a

--- Videoclub/src/main/java/Interfaz/Interface.java
+++ Videoclub/src/main/java/Interfaz/Interface.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package Interfaz;
-
+import java.util.Scanner;
 /**
  *
  * @author sebas
  */
+
+
 public class Interface {
-    
+    public static void login(){
+        Scanner entrada = new Scanner(System.in);
+        int i;
+        
+        do{
+            System.out.println("Porfavor, seleccione tipo de Usuario: ");
+            System.out.println("1)Cliente");
+            System.out.println("2)Trabajador");
+        
+            i = entrada.nextInt();
+            switch(i){
+                case 1:
+                    ;
+                case 2:
+                    ;
+                default:
+                    if(i != 0) System.out.println("Ingrese una opción válida...");
+            }
+            
+        }while(i != 0);
+    }
 }
--- Videoclub/src/main/java/Main.java
+++ Videoclub/src/main/java/Main.java
@@ -3,11 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import Interfaz.Interface;
 /**
  *
  * @author sebas
  */
 public class Main {
+    public static void main(String[] args){
+        
+        Interfaz.Interface.login();
+        
+    }
     
 }
