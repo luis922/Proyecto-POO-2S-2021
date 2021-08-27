4a1e59b55081202c68505540525e2bd28fe0c07c
JJ1312 <yeiyeithirteen@gmail.com>
2021-08-26 21:31:12 -0400

    Update Main.java

--- Videoclub/src/main/java/Main.java
+++ Videoclub/src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.io.*;
  */
 public class Main {
     public static void main(String[] args) throws Exception{
+	VideoClub videoClub1 = new VideoClub();
 //------------------------------Lectura de datos desde archivos------------------------------
         String linea;
         int qDatosPeliculas = 11;
