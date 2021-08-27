1add1b1899ae47ef9827863a4a7545987b2d0d7b
JJ1312 <yeiyeithirteen@gmail.com>
2021-08-27 19:21:56 -0400

    Add files via upload

--- Videoclub/src/main/java/Main.java
+++ Videoclub/src/main/java/Main.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Clases;
+
 import java.util.*; //Scanner, Arrays
 import java.io.*;
-/**
- *
- * @author sebas
- */
+
+
 public class Main {
     public static void main(String[] args) throws Exception{
 	VideoClub videoClub1 = new VideoClub();
@@ -29,18 +24,18 @@ public class Main {
 	    linea = scPel.nextLine();
 	    arrayLineaPeliculas = linea.split("\t");
 	    //Guardar Datos en array de Peliculas
-	    pelicula = new Pelicula();
+	    Pelicula pelicula = new Pelicula();
 	    pelicula.setNombre(arrayLineaPeliculas[0]);
-	    pelicula.setExistencia(arrayLineaPeliculas[1]);
-	    pelicula.setDisponilbes(arrayLineaPeliculas[2]);
-	    pelicula.setValuacion(arrayLineaPeliculas[3]);
-	    pelicula.setAñoEstreno(arrayLineaPeliculas[4]);
-	    pelicula.setDuracionMin(arrayLineaPeliculas[5]);
+	    pelicula.setExistencias(Short.parseShort(arrayLineaPeliculas[1]));
+	    pelicula.setDisponibles(Short.parseShort(arrayLineaPeliculas[2]));
+	    pelicula.setValuacion(Float.parseFloat(arrayLineaPeliculas[3]));
+	    pelicula.setAñoEstreno(Short.parseShort(arrayLineaPeliculas[4]));
+	    pelicula.setDuraciónMin(Short.parseShort(arrayLineaPeliculas[5]));
 	    pelicula.setSinopsis(arrayLineaPeliculas[6]);
 	    pelicula.setCalidad(arrayLineaPeliculas[7]);
-	    pelicula.setDirector(arrayLineaPeliculas[8].split('_'));
-	    pelicula.setActores(arrayLineaPeliculas[9].split('_'));
-	    pelicula.setGeneros(arrayLineaPeliculas[10].split('_'));
+	    pelicula.setDirector(arrayLineaPeliculas[8].split(" "));
+	    pelicula.setActores(arrayLineaPeliculas[9].split(" "));
+	    pelicula.setGeneros(arrayLineaPeliculas[10].split(" "));
 	    listaPeliculas.add(pelicula);
 	}
 	videoClub1.setListaPeliculas(listaPeliculas);
@@ -52,18 +47,17 @@ public class Main {
 	    linea = scCli.nextLine();
 	    arrayLineaClientes = linea.split("\t");
 	    //Guardar Datos en array de Clientes: [0]=noimbre, [1]=rut, [2]=historial, [3]=pelPosesion, [4]=Deuda
-	    cliente = new Cliente();
+	    Cliente cliente = new Cliente();
 	    cliente.setNombre(arrayLineaClientes[0]);
-	    cliente.setrut(arrayLineaClientes[1]);
+	    cliente.setRut(arrayLineaClientes[1]);
 	    cliente.setHistorial(arrayLineaClientes[2].split("_"));
 	    cliente.setPeliculasPosesion(arrayLineaClientes[3].split("_"));
-	    cliente.setDeuda(arrayLineaClientes[4]);
+	    cliente.setDeuda(Integer.parseInt(arrayLineaClientes[4]));
 	}
 	videoClub1.setListaCliente(listaClientes);       
 //----------------------------------------------------------------------------------------------  
         Scanner entrada = new Scanner(System.in);
         int opción;
-        
         do{
             Interfaz.Interface.login();
             opción = entrada.nextInt();
