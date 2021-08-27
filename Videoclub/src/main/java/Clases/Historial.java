a5451629330e7919cb6783c229d908770158f5b5
Luis <luisromeroquiroz235@gmail.com>
2021-08-27 18:54:25 -0400

    nueva clase

---
+++ Videoclub/src/main/java/Clases/Historial.java
@@ -0,0 +1,54 @@
+package Clases;
+
+public class Historial {
+    Pelicula film;
+    float valoracion; //Numero de estrellas con la que el cliente valoro el film de 1 a 5
+    String fechaArriendo;
+    String fechaEntrega;
+    boolean entregado; //TRUE si la pelicula fue devuelta, False en el caso contrario
+    
+    public Historial(){
+        entregado = true;
+    }
+
+    public Pelicula getFilm() {
+        return film;
+    }
+
+    public void setFilm(Pelicula film) {
+        this.film = film;
+    }
+
+    public float getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(float valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public String getFechaArriendo() {
+        return fechaArriendo;
+    }
+
+    public void setFechaArriendo(String fechaArriendo) {
+        this.fechaArriendo = fechaArriendo;
+    }
+
+    public String getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(String fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public boolean isEntregado() {
+        return entregado;
+    }
+
+    public void setEntregado(boolean entregado) {
+        this.entregado = entregado;
+    }    
+    
+}
