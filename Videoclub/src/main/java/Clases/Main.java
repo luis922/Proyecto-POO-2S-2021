a8ea07df27faee7fed34cc23a65a162bb36f3afe
JJ1312 <yeiyeithirteen@gmail.com>
2021-10-25 19:47:42 -0300

    Arreglo de tabulaciones

--- Videoclub/src/main/java/Clases/Main.java
+++ Videoclub/src/main/java/Clases/Main.java
@@ -8,12 +8,12 @@ import Menú.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-	    VideoClub videoClub1 = new VideoClub();
+	VideoClub videoClub1 = new VideoClub();
 //------------------Lectura de datos desde archivos--------------------------------
         Funciones.LeerArchivoPeliculas(videoClub1);
         Funciones.LeerArchivoClientes(videoClub1);
         Funciones.LeerArchivoTrabajadores(videoClub1);
 //-------------------Menu  por consola------------------------------
-	    MenuTienda.inicioMenu(videoClub1);
+	MenuTienda.inicioMenu(videoClub1);
     }
 }
